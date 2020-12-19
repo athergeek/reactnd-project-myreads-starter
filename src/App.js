1776905811f07e9a987d3b4a0c0bdbfd55ffc75a
ather <contact@risenruns.com>
2020-12-19 17:25:31 -0600

    Extracted state data from static html

--- src/App.js
+++ src/App.js
@@ -10,7 +10,67 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false,
+    bookShelves: [
+      {
+        shelveTitle: "Currently Reading",
+        books: [
+            {
+              coverImage: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
+              cuurentShelve: "Currently Reading",
+              title: "To Kill a Mockingbird",
+              Author: "Harper Lee"
+            },
+            {
+              coverImage: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
+              cuurentShelve: "Currently Reading",
+              title: "Ender's Game",
+              Author: "Orson Scott Card"
+            }				
+        ]
+      },
+      {
+        shelveTitle: "Want to Read",
+        books: [
+            {
+              coverImage: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
+              cuurentShelve: "Want to Read",
+              title: "1776",
+              Author: "David McCullough"
+            },
+            {
+              coverImage: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
+              cuurentShelve: "Want to Read",
+              title: "Harry Potter and the Sorcerer's Stone",
+              Author: "J.K.Rowling"
+            }				
+        ]
+      },
+      {
+        shelveTitle: "Read",
+        books: [
+            {
+              coverImage: "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api",
+              cuurentShelve: "Read",
+              title: "The Hobbit",
+              Author: "J.R.R. Tolkien"
+            },
+            {
+              coverImage: "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api",
+              cuurentShelve: "Read",
+              title: "Oh, the Places You'll Go!",
+              Author: "Seuss"
+            },
+            {
+              coverImage: "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api",
+              cuurentShelve: "Read",
+              title: "The Adventures of Tom Sawyer",
+              Author: "Mark Twain"
+            }				
+
+        ]
+      }             
+    ]    
   }
 
   render() {
